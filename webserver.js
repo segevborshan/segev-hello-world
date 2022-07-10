7b1e865c2e65013131f6f5042b4e7050d54a708e
segevborshan <104018375+segevborshan@users.noreply.github.com>
2022-07-10 20:06:58 +0300

    Create webserver.js

---
+++ webserver.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const PORT = 80;
+const HOST = '0.0.0.0';
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World, segev example');
+});
+
+app.listen(PORT, HOST);
