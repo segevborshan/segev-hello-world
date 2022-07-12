5c0a989d3119fa2d28d34dc781bfd125913b22ca
segevborshan <104018375+segevborshan@users.noreply.github.com>
2022-07-12 03:35:48 +0300

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -3,7 +3,7 @@ const PORT = 80;
 const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - segev-example');
+  res.send('Hello World -- segev-example');
 });
 
 app.listen(PORT, HOST);
